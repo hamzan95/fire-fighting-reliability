fb13f70c863aae7ffe2087365b20dac51a4ccb56
Hamza Bounezra <hamzabounezra@gmail.com>
2025-06-10 14:43:05 +0300

    Update substation.py

--- src/models/substation.py
+++ src/models/substation.py
@@ -3,7 +3,6 @@ from datetime import datetime
 
 class Substation(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    substation_id = db.Column(db.String(20), unique=True, nullable=False)
     name = db.Column(db.String(100), unique=True, nullable=False)
     coverage_status = db.Column(db.String(50), nullable=False)
     created_at = db.Column(db.DateTime, default=datetime.utcnow)
